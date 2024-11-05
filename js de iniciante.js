4e7d98958dd7751fc201abd595df4aa75d955e97
Rogerio Junior <161977657+senhordev01@users.noreply.github.com>
2024-11-05 02:07:24 -0300

    Add files via upload

---
+++ js de iniciante.js
@@ -0,0 +1,12 @@
+var a1 = 1;
+var a2 = 2;
+var a3 = 3;
+var a4 = 4;
+var a5 = 5;
+var aX = 1000;
+let soma = a1 + a2 + a3 + a4 + a5 + aX;
+console.log("a soma dos valores é: " + soma);
+console.log("a potência de a5 elevado a 5 é: " + a5 ** 5);
+console.log("o resto da divisao de 5 / 2 é: " + a5 % 2);
+console.log(a5 + (10 * 1000));
+console.log("ola, mundo!");
